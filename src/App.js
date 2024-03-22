984242b896d46957d2ccdee4adadde0372627f9e
Aankit Pokhrel <2001aankit@gmail.com>
2024-03-22 14:12:51 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>My Name is Aankit</p>
+        <p>
+          Hello World <br /> My Name is Aankit
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
